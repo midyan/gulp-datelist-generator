4e23a0efd6ddb55d1d13e15e5f58af3f6918caa0
yan <yannunes@gmail.com>
2017-01-30 17:21:46 -0300

    Minor text fixes

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,18 @@ var gulp = require('gulp')
 var fs = require('fs')
 var argv = require('yargs').argv
 
-
 var generateSequence = (start, end) => {
 
   var sequence = []
 
-  if( end < start ){
+  if (end < start) {
     var temp = end
     end = start
     start = temp
   }
 
-  for (var i = start; i<=end; i++) {
-    var index = i<10? 0 + i.toString() : i
+  for ( var i = start; i <= end; i++) {
+    var index = i < 10 ? 0 + i.toString() : i
     sequence.push(index)
   }
 
@@ -26,10 +25,8 @@ gulp.task('generate-date-list', () => {
   var startYear = parseInt(argv.start)
   var endYear = parseInt(argv.end)
 
-  console.log(startYear)
-
-  if( typeof startYear !== 'number' || (startYear%1) !== 0 ) return console.log( new Error('End and Start years must be intergers!!') )
-  if( typeof endYear !== 'number' || (endYear%1) !== 0 ) return console.log( new Error('End and Start years must be intergers!!') )
+  if (typeof startYear !== 'number' || (startYear % 1) !== 0) return console.log(new Error('End and Start years must be intergers!!'))
+  if (typeof endYear !== 'number' || (endYear % 1) !== 0) return console.log(new Error('End and Start years must be intergers!!'))
 
   var dates = []
 
@@ -37,18 +34,15 @@ gulp.task('generate-date-list', () => {
   var months = generateSequence(1, 12)
   var years = generateSequence(startYear, endYear)
 
-  for(var index_ano = 0; index_ano<years.length; index_ano++) {
-    for(var index_mes = 0; index_mes<months.length; index_mes++) {
-      for(var index_dia = 0; index_dia<days.length; index_dia++) {
-
-        dates.push( days[index_dia].toString() + months[index_mes].toString() + years[index_ano].toString() )
-
+  for ( var index_year = 0; index_year < years.length; index_year++ ) {
+    for ( var index_month = 0; index_month < months.length; index_month++ ) {
+      for ( var index_day = 0; index_day < days.length; index_day++ ) {
+        dates.push( days[index_day].toString() + months[index_month].toString() + years[index_year].toString() )
       }
     }
   }
 
-  console.log(dates.length + " dates were generated.\n")
+  console.log(dates.length + ' dates were generated.\n')
 
-  fs.writeFileSync('datelist_'+startYear+'_'+endYear+'.txt', dates.join('\n'))
-
+  fs.writeFileSync('datelist_' + startYear + '_' + endYear + '.txt', dates.join('\n'))
 })
