5ee86a6bbf0ee56edfda8bb9603783d73438c49c
yan <yannunes@gmail.com>
2017-01-30 15:24:23 -0300

    Added gitgnore and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gulp-datelist-generator
-Simples datelist generator that generates every possible date between two given years
+Simples datelist generator that generates every possible date between two given years.<br>
+It does assume that every month has 31 days because, lets be honest, it will make no difference.
+
+#How to use it:
+
+Just run
+```sh
+$ gulp generate-date-list --start START_YEAR --end END_YEAR
+```
+
+It will create a datelist.txt file with all the dates inside.
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp')
+var fs = require('fs')
+var argv = require('yargs').argv
+
+
+var generateSequence = (start, end) => {
+
+  var sequence = []
+
+  for (var i = start; i<=end; i++) {
+    var index = i<10? 0 + i.toString() : i
+    sequence.push(index)
+  }
+
+  return sequence
+}
+
+gulp.task('generate-date-list', () => {
+
+  var dates = []
+
+  var startYear = argv.start
+  var endYear = argv.end
+
+  var days = generateSequence(1, 31)
+  var months = generateSequence(1, 12)
+  var years = generateSequence(startYear, endYear)
+
+  for(var index_ano = 0; index_ano<years.length; index_ano++) {
+    for(var index_mes = 0; index_mes<months.length; index_mes++) {
+      for(var index_dia = 0; index_dia<days.length; index_dia++) {
+
+        dates.push( days[index_dia].toString() + months[index_mes].toString() + years[index_ano].toString() )
+
+      }
+    }
+  }
+
+  fs.writeFileSync('datelist.txt', dates.join('\n'))
+
+})
