fdcdbe0d5984c6f045ef4df191c12c651c989875
yan <yannunes@gmail.com>
2017-02-07 13:41:02 -0300

    Months now have the correct number of days, including leap years

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp')
 var fs = require('fs')
 var argv = require('yargs').argv
+// Jan  Feb    Mar   Apr    May   Jun    July  Aug   Sept  Oct    Nov    Dec
+var has31 = [true, false, true, false, true, false, true, true, false, true, false, true]
 
 var generateSequence = (start, end) => {
 
@@ -34,16 +36,56 @@ gulp.task('generate-date-list', () => {
   var months = generateSequence(1, 12)
   var years = generateSequence(startYear, endYear)
 
-  for ( var index_year = 0; index_year < years.length; index_year++ ) {
-    for ( var index_month = 0; index_month < months.length; index_month++ ) {
-      for ( var index_day = 0; index_day < days.length; index_day++ ) {
-        dates.push( days[index_day].toString() + months[index_month].toString() + years[index_year].toString() )
+  for ( var index_year = 0; index_year < years.length; index_year++) {
+
+
+    if (years[index_year] % 4 === 0 && years[index_year] % 100 != 0 || years[index_year] % 400 == 0 && years[index_year] % 100 != 0) { // BISEXTO
+      for ( var index_month = 0; index_month < months.length; index_month++) {
+        if (has31[index_month]) { // 31
+
+          for ( var index_day = 0; index_day < days.length; index_day++) {
+            dates.push(days[index_day].toString() + months[index_month].toString() + years[index_year].toString())
+          }
+        } else if (index_month == 1) { // feb
+
+          for ( var index_day = 0; index_day < days.length; index_day++) {
+            dates.push(days[index_day].toString() + months[index_month].toString() + years[index_year].toString())
+            if (days[index_day] == 29) index_day = days.length
+          }
+        } else { // 30
+
+          for ( var index_day = 0; index_day < days.length; index_day++) {
+            dates.push(days[index_day].toString() + months[index_month].toString() + years[index_year].toString())
+            if (days[index_day] == 30) index_day = days.length
+          }
+        }
+      }
+    } else { // NORMAL
+
+      for ( var index_month = 0; index_month < months.length; index_month++) {
+        if (has31[index_month]) { // 31
+
+          for ( var index_day = 0; index_day < days.length; index_day++) {
+            dates.push(days[index_day].toString() + months[index_month].toString() + years[index_year].toString())
+          }
+        } else if (index_month == 1) { // feb
+
+          for ( var index_day = 0; index_day < days.length; index_day++) {
+            dates.push(days[index_day].toString() + months[index_month].toString() + years[index_year].toString())
+            if (days[index_day] == 28) index_day = days.length
+          }
+        } else { // 30
+
+          for ( var index_day = 0; index_day < days.length; index_day++) {
+            dates.push(days[index_day].toString() + months[index_month].toString() + years[index_year].toString())
+            if (days[index_day] == 30) index_day = days.length
+          }
+        }
       }
     }
   }
 
-  console.log(dates.length + ' dates were generated.)
+  console.log(dates.length + ' dates were generated')
 
   fs.writeFileSync('datelist_' + startYear + '_' + endYear + '.txt', dates.join('\n'))
-  
 })
